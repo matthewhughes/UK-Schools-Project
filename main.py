735c12cf98535ed2593e9f49f94389ba2d2870b3
Matthew Hughes <me@matthewhughes.co.uk>
2013-11-19 14:16:06 +0000

    Converting postcode to lat-long. Ideal for police stats

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import sys
 import csv
 import json
 
+def ConvertPostCodeToLatAndLongitude(postcode):
+    geodata = requests.get("http://mapit.mysociety.org/postcode/" + postcode).content
+    geodata = json.loads(geodata)
+    latlong = [geodata['wgs84_lat'], geodata['wgs84_lon']]
+    return latlong
+
 def main():
     print "This code will eventually do something useful"
 
