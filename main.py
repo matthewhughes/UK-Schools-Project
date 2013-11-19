606b632af45f1022c70341742f23c8438f371614
Matthew Hughes <me@matthewhughes.co.uk>
2013-11-19 15:25:11 +0000

    Pulling from police stats. General weirdness

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import sys
 import csv
 import json
 
+def GetCrimeData(latitude, longitude):
+    statistics = requests.get("http://data.police.uk/api/crimes-at-location?date=2012-02&lat=" + latitude + "&lng=" + longitude).content
+    print statistics
+
 def ConvertPostCodeToLatAndLongitude(postcode):
     geodata = requests.get("http://mapit.mysociety.org/postcode/" + postcode).content
     geodata = json.loads(geodata)
@@ -11,6 +15,10 @@ def ConvertPostCodeToLatAndLongitude(postcode):
 
 def main():
     print "This code will eventually do something useful"
+    latlong = ConvertPostCodeToLatAndLongitude('mk181hy')
+    print str(latlong[0])
+    print str(latlong[1])
+    GetCrimeData(str(latlong[0]), str(latlong[1]))
 
 if __name__ == '__main__':
     main()
